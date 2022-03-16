ba37e2dda1d05cc9b809dfdd1b8c556d9b0b3810
BT_1E10_17 <sample@bths.edu>
2022-03-16 12:28:27 -0400

    Initial commit

---
+++ src/PhysicalDrive.java
@@ -0,0 +1,9 @@
+public class PhysicalDrive {
+    private String name;
+    private int size;
+
+    public PhysicalDrive(String name, int size){
+        this.name = name;
+        this.size = size;
+    }
+}
---
+++ src/SUPERCLASS.java
@@ -0,0 +1,12 @@
+import java.util.UUID;
+
+public class SUPERCLASS {
+
+    private String name;
+    private UUID UUID;
+
+    public SUPERCLASS(String name, UUID UUID){
+        this.name = name;
+        this.UUID = UUID.randomUUID();
+    }
+}
