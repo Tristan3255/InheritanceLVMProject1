c42a0c31df3128fcf2b6447b62d3340224b1f576
Tristan3255 <tristanc16@nycstudents.net>
2022-03-30 23:52:52 -0400

    Completed the ability to add Physical Drives to a list as well as listing the list.

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<PhysicalDrive> PHD = new ArrayList<PhysicalDrive>();
+
+        System.out.println("Welcome to the LMV Creator");
+        Scanner input = new Scanner(System.in);
+        String userChoice = "";
+        while(!(userChoice.equals("exit"))) {
+            System.out.println("Enter one of the following commands\n-install-drive/list-drives\n-pvcreate/pvlist\n-vgcreate/vgextend/vglist\n-lvcreate/lvlist\n-exit");
+            userChoice = input.nextLine();
+            if(userChoice.equals("install-drive")){
+                boolean error = false;
+                System.out.print("install-drive ");
+                String pDrive = input.nextLine();
+                String name = pDrive.substring(0, pDrive.indexOf(" "));
+                int size = Integer.parseInt(pDrive.substring(pDrive.indexOf(" ") + 1, pDrive.length()-1));
+                PhysicalDrive drive = new PhysicalDrive(name,size);
+
+                for(int i = 0; i < PHD.size(); i++){
+                    if(PHD.get(i).getName().equals(name)){
+                        error = true;
+                    }
+                }
+
+                if(error == false){
+                    PHD.add(drive);
+                    System.out.println();
+                    System.out.println("Installation Complete");
+                    System.out.println();
+                }
+                else{
+                    System.out.println();
+                    System.out.println("This drive has already been installed");
+                    System.out.println();
+                }
+
+            }
+            if(userChoice.equals("list-drives")){
+                System.out.println();
+                for(int x = 0; x < PHD.size(); x++){
+                    System.out.print(PHD.get(x).getName() + " [" + PHD.get(x).getSize() + "G]");
+                    System.out.println();
+                }
+                System.out.println();
+            }
+
+        }
+
+    }
 }
